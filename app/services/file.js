f968f7a45b74f26c807aa282c36265c025022fe3
Ahmad Syangkan Syarip <ahmadssyarif27@gmail.com>
2025-12-15 21:54:56 +0700

    menambahkan sweetalert dan notyf saat proses delete

--- app/services/file.js
+++ app/services/file.js
@@ -1,4 +1,4 @@
-import { getData, saveData } from "./core.js";
+import { getData, saveData, notyf } from "./core.js";
 
 const keyMahasiswa = "mahasiswa";
 
@@ -101,7 +101,7 @@ const renderSecondTable = () => {
     <td>
     <button type="button"
     class="btn btn-icons btn-rounded btn-danger delete-student"
-    onclick="deleteConfirm(${x.id})"
+    onclick="confirmDelete(${x.id})"
     title="Hapus Mahasiswa">
     <i class="mdi mdi-delete"></i>
     </button>
@@ -109,12 +109,23 @@ const renderSecondTable = () => {
     </tr>`;
   });
 };
-const deleteConfirm = (id) => {
-  const isDelete = confirm("Yakin mau dihapus?");
-  if (isDelete) {
-    deleteStudent(id);
-  }
-};
+function confirmDelete(id) {
+  Swal.fire({
+    title: " Hapus data",
+    text: "Anda yakin mau menghapus data ini?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Ya, hapus",
+    cancelButtonText: "Batal",
+    confirmButtonColor: "#dc3545",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      deleteStudent(id);
+      notyf.success("Data berhasil dihapus");
+    }
+  });
+}
+
 
 const deleteStudent = (id) => {
   const index = mahasiswa.findIndex((i) => i.id === id);
@@ -125,7 +136,7 @@ const deleteStudent = (id) => {
   }
 };
 
-window.deleteConfirm = deleteConfirm;
+window.confirmDelete = confirmDelete;
 
 const secondTable = document.getElementById("second-table");
 document.addEventListener("DOMContentLoaded", () => {
